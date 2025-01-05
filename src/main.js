631274552b57014838ab99fcdfcefdb3cb61df41
sophie <jo.huichow@163.com>
2025-01-05 21:22:14 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+if (process.env.NODE_ENV !== 'production') {
+  Vue.config.devtools = true; // 确保启用 devtools
+}
 
 new Vue({
   router,
